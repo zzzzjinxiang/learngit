6ffffdbe9c2d7fd417400d05aaa0304a6b06933c
John <zjx1574@qq.com>
2019-05-30 16:24:02 +0800

    dev commit

--- stackTake.java
+++ stackTake.java
@@ -1,11 +1,10 @@
 public class stackTake{
 	private volatile int m =0;
-	private String sym ;
 	public int cal(){
 		synchronized(this){
 		int i = 12;
 		int j = 13;
-		return i+j;
+		return i+j*i;
 		}
 	}
 }
