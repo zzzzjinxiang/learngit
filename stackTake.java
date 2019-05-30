684b012cc694f2eb9e3b11e40756a0a193c4f02e
John <zjx1574@qq.com>
2019-05-30 16:57:58 +0800

    master

--- stackTake.java
+++ stackTake.java
@@ -1,14 +1,10 @@
 public class stackTake{
 	private volatile int m =0;
-	private lock lock = new lock();
-	private condition con;
 	public int cal(){
-		lock.lock();
 		synchronized(this){
 		int i = 12;
 		int j = 13;
 		}
-		lock.unlock();
 		return i*j+j*i;
 	}
 }
